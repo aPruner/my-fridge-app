8fb204eda201e2bf189e1ef8eeaadfe54bfeea49
aPruner <adampruner99@gmail.com>
2020-05-29 21:43:08 -0700

    add some boilerplate for a basic loading screen

---
+++ lib/screens/loading.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<LoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}

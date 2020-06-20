68d99734d3855c7d5211f227a47caac3f20b7d37
aPruner <adampruner99@gmail.com>
2020-06-20 16:38:28 -0700

    init authService in main.dart to pass down to screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'screens/login.dart';
+import 'services/auth.dart';
 
 void main() {
   runApp(MyFridgeApp());
@@ -8,9 +9,12 @@ void main() {
 
 class MyFridgeApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  final AuthService authService = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    // Set screen orientation to only vertical
+    // Set screen orientation to only vertical (no rotation happens when phone is re-oriented to landscape)
     // TODO: Look into whether or not this works on iOS as well
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
@@ -23,7 +27,7 @@ class MyFridgeApp extends StatelessWidget {
       ),
       home: Scaffold(
         resizeToAvoidBottomInset: false,
-        body: LoginScreen(),
+        body: LoginScreen(authService: authService),
       ),
     );
   }

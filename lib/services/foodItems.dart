76c5bf19e54c871e5ceb4172815086be7d69932b
aPruner <adampruner99@gmail.com>
2020-08-16 13:26:40 -0700

    write gql queries and mutations in service classes

--- lib/services/foodItems.dart
+++ lib/services/foodItems.dart
@@ -1,1 +1,46 @@
-// TODO: this file will contain graphql calls for CRUD related to food items
+class FoodItemsService {
+  final String getFoodItemsByHouseholdIdQuery = """
+  query {
+    foodItems(householdId: \$householdId) {
+      id,
+      name,
+      category,
+      amount,
+      unit,
+      householdId,
+      shoppingListId
+    }
+  }
+  """;
+
+  final String createFoodItemMutation = """
+  mutation {
+    createFoodItem(
+      name: \$name,
+      category: \$category,
+      amount: \$amount,
+      unit: \$unit,
+      householdId: \$householdId,
+      shoppingListId: \$shoppingListId
+    ) {
+      id
+    }
+  }
+  """;
+
+  final String updateFoodItemMutation = """
+  mutation {
+    updateFoodItem(
+      id: \$id,
+      name: \$name,
+      category: \$category,
+      amount: \$amount,
+      unit: \$unit,
+      householdId: \$householdId,
+      shoppingListId: \$shoppingListId
+    ) {
+      id
+    }
+  }
+  """;
+}
--- lib/services/shoppingLists.dart
+++ lib/services/shoppingLists.dart
@@ -1,1 +1,38 @@
-// TODO: this file will contain graphql calls for CRUD related to shopping lists
+class ShoppingListsService {
+  final String getShoppingListsByHouseholdIdQuery = """
+  query {
+    shoppingLists(householdId: \$householdId) {
+      id,
+      name,
+      userId,
+      householdId,
+      createdAt
+    }
+  }
+  """;
+
+  final String createShoppingListMutation = """
+  mutation {
+    createShoppingList(
+      name: \$name,
+      userId: \$userId,
+      householdId: \$householdId,
+    ) {
+      id
+    }
+  }
+  """;
+
+  final String updateShoppingListMutation = """
+  mutation {
+    updateShoppingList(
+      id: \$id,
+      name: \$name,
+      userId: \$userId,
+      householdId: \$householdId,
+    ) {
+      id
+    }
+  }
+  """;
+}

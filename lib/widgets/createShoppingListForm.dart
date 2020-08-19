4fd0c33bed78955b2a56dd45f37191a8c92b8ec4
aPruner <adampruner99@gmail.com>
2020-08-18 20:34:16 -0700

    Create shopping list requests work

--- lib/widgets/createShoppingListForm.dart
+++ lib/widgets/createShoppingListForm.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import '../services/shoppingLists.dart';
 
 class CreateShoppingListForm extends StatefulWidget {
   CreateShoppingListForm({Key key}) : super(key: key);
@@ -9,148 +11,170 @@ class CreateShoppingListForm extends StatefulWidget {
 
 class _CreateShoppingListFormState extends State<CreateShoppingListForm> {
   final _formKey = GlobalKey<FormState>();
-  String dropdownValue = '(Fridge) MyFridge';
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _descriptionController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Container(
-        padding: EdgeInsets.all(
-          20.0,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Text(
-              'Create a Shopping List',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 42.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: 30.0,
-                vertical: 30.0,
-              ),
-              child: TextFormField(
-                cursorColor: Colors.black,
+    return GraphQLConsumer(builder: (GraphQLClient client) {
+      return Form(
+        key: _formKey,
+        child: Container(
+          padding: EdgeInsets.all(
+            20.0,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text(
+                'Create a Shopping List',
                 style: TextStyle(
-                  fontSize: 20,
+                  color: Colors.black,
+                  fontSize: 42.0,
+                  fontWeight: FontWeight.w700,
                 ),
-                decoration: const InputDecoration(
-                  labelText: 'Name',
-                  hintText: 'e.g: Weekly Staples',
-                  labelStyle: TextStyle(
-                    fontSize: 26.0,
-                    color: Colors.black,
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                  errorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red),
-                  ),
-                  focusedErrorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red),
-                  ),
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                left: 30.0,
-                right: 30.0,
-                bottom: 30.0,
-              ),
-              child: TextFormField(
-                cursorColor: Colors.black,
-                style: TextStyle(
-                  fontSize: 20,
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 30.0,
+                  vertical: 30.0,
                 ),
-                decoration: const InputDecoration(
-                  labelText: 'Description',
-                  hintText: 'e.g: Everything we eat on a daily basis',
-                  labelStyle: TextStyle(
-                    fontSize: 26.0,
-                    color: Colors.black,
+                child: TextFormField(
+                  cursorColor: Colors.black,
+                  style: TextStyle(
+                    fontSize: 20,
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                  errorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red),
-                  ),
-                  focusedErrorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red),
-                  ),
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                ButtonTheme(
-                  minWidth: 120.0,
-                  height: 60.0,
-                  child: RaisedButton(
-                    onPressed: () {
-                      if (_formKey.currentState.validate()) {
-                        // Process data
-                      }
-                    },
-                    child: Text(
-                      'Create',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 26.0,
-                      ),
+                  decoration: const InputDecoration(
+                    labelText: 'Name',
+                    hintText: 'e.g: Weekly Staples',
+                    labelStyle: TextStyle(
+                      fontSize: 26.0,
+                      color: Colors.black,
                     ),
-                    color: Colors.green,
-                  ),
-                ),
-                ButtonTheme(
-                  minWidth: 120.0,
-                  height: 60.0,
-                  child: RaisedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text(
-                      'Cancel',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 26.0,
-                      ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
                     ),
-                    color: Colors.grey,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
                   ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  controller: _nameController,
                 ),
-              ],
-            ),
-          ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: 30.0,
+                  right: 30.0,
+                  bottom: 30.0,
+                ),
+                child: TextFormField(
+                  cursorColor: Colors.black,
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                  decoration: const InputDecoration(
+                    labelText: 'Description',
+                    hintText: 'e.g: Everything we eat on a daily basis',
+                    labelStyle: TextStyle(
+                      fontSize: 26.0,
+                      color: Colors.black,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  controller: _descriptionController,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  ButtonTheme(
+                    minWidth: 120.0,
+                    height: 60.0,
+                    child: RaisedButton(
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          QueryResult result = await client.mutate(
+                            MutationOptions(
+                                documentNode: gql(ShoppingListsService
+                                    .createShoppingListMutation),
+                                variables: {
+                                  'name': _nameController.value.text,
+                                  'description':
+                                      _descriptionController.value.text,
+                                  // TODO: Get these from the authenticated user!
+                                  'householdId': 1,
+                                  'userId': 1,
+                                }),
+                          );
+                          if (result.hasException) {
+                            print(result.exception);
+                          }
+                          // TODO: Do a re-query of shopping lists for the shopping lists screen here
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: Text(
+                        'Create',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 26.0,
+                        ),
+                      ),
+                      color: Colors.green,
+                    ),
+                  ),
+                  ButtonTheme(
+                    minWidth: 120.0,
+                    height: 60.0,
+                    child: RaisedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text(
+                        'Cancel',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 26.0,
+                        ),
+                      ),
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }

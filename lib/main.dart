526d0fb6a8eec37ab9d27a66f0d0ca6ea176770b
aPruner <adampruner99@gmail.com>
2020-11-01 17:49:17 -0800

    Use logo for app icon with the flutter icons tool

--- lib/main.dart
+++ lib/main.dart
@@ -27,12 +27,27 @@ class MyFridgeApp extends StatelessWidget {
       ],
     );
 
+    Map<int, Color> colorSwatch = {
+      50: Colors.white,
+      100: Colors.white,
+      200: Colors.white,
+      300: Colors.white,
+      400: Colors.white,
+      500: Colors.white,
+      600: Colors.white,
+      700: Colors.white,
+      800: Colors.white,
+      900: Colors.white,
+    };
+
     return GraphQLProvider(
       client: GraphqlService.getGraphQLClient(),
       child: MaterialApp(
         title: 'MyFridge',
         theme: ThemeData(
-          primarySwatch: Colors.green,
+          primarySwatch: MaterialColor(0xFFFFFFFF, colorSwatch),
+          // Set DMSans as the default font
+          fontFamily: 'DMSans',
         ),
         home: Scaffold(
           body: LoginScreen(authService: authService),
